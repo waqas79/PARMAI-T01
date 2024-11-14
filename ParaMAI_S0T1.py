af593c0198a5168f1b96a7e54502bcd4997f77d9
waqas7931 <waqas92.ee@gmail.com>
2024-11-14 09:42:52 +0100

    2024Nov14_tart1 A working

--- ParaMAI_S0T1.py
+++ ParaMAI_S0T1.py
@@ -1,6 +1,16 @@
-def sequential_max(arr):
-    return max(arr)
+array_x = [2, 4, 6, 8, 1, 3, 5, 7]
+print("Array_x : ",array_x)
 
-arr = [2, 4, 6, 8, 1, 3, 5, 7]
-print("Sequential Maximum:", sequential_max(arr))
-print(arr,arr)
+def Array_prefix_sum(array_x):
+    sum = [] # blank list for saving sum
+    sum_counter = 0 # init temp_sum
+
+    for num in array_x:
+        sum_counter += num
+        sum.append(sum_counter)
+    return sum
+
+
+
+result = Array_prefix_sum(array_x)
+print("prefix sum of array_x", result)
